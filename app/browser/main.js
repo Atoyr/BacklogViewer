93a6a7057d598dedd298b3b3398d68b8ed8a58fc
r.uchiyama <r.uchiyama@sint.co.jp>
2018-05-23 12:28:15 +0900

    feat:httpリクエストの実行

--- app/browser/main.js
+++ app/browser/main.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const {app, BrowserWindow} = require('electron');
 const debug = /--debug/.test(process.argv[2]);
-
+import {ipcMain} from "electron";
+import * as backlogApi from '../components/backlogApi.jsx';
+import httpHelper from '../components/httpHelper.jsx';
 var mainWindow = null;
 
 
@@ -44,3 +46,26 @@ function initialize () {
 
 // Handle Squirrel on Windows startup events
 initialize()
+
+ipcMain.on('async-get-myself',function(event,arg){
+  httpHelper('http://google.com',(status,header,body) => {  event.sender.send('async-get-myself-reply',body);})
+  // let apiKey = arg.apiKey;
+  // let spaceKey = arg.spaceKey;
+  // backlog.getMyself(spaceKey,apiKey,(body) => {
+  //   event.sender.send('async-get-myself-reply',body);
+  // });
+
+  // const {net} = require('electron');
+  // const request = net.request('http://google.com');
+  // request.on('response', (response) => {
+  //     let body = '';
+  //     response.on('data', (chunk) => {
+  //         body += chunk;
+  //     });
+  //     response.on('end', () => {
+  //         event.sender.send('async-get-myself-reply',body);
+  //     });
+  // });
+  // request.end();
+
+});
--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,4 +1,12 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import * as electron from 'electron'
+ReactDOM.render(<div><button id='bt'>GET IT</button></div>, document.getElementById("app"))
+const bt = document.getElementById('bt');
+bt.addEventListener('click',function(){
+    electron.ipcRenderer.send('async-get-myself',{spaceKey:'sint01',apiKey:'hogehoge'})
+});
 
-ReactDOM.render(<div>This is React component</div>, document.getElementById("app"))
+electron.ipcRenderer.on('async-get-myself-reply', function(event, arg) {
+    ReactDOM.render(`<div>${arg}</div>`, document.getElementById("app"))
+});
