c8687f6d716238e96998e2af2431b217f13d8d14
Atoyr <lessironglance@gmail.com>
2018-05-24 09:06:28 +0900

    Revert "Feature/add json storage (#2)"
    This reverts commit d657b60a73554e95a954e15c4d20d6d2b25ef22d.

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,12 +1,4 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import * as electron from 'electron'
-ReactDOM.render(<div><button id='bt'>GET IT</button></div>, document.getElementById("app"))
-const bt = document.getElementById('bt');
-bt.addEventListener('click',function(){
-    electron.ipcRenderer.send('async-get-myself',{spaceKey:'sint01',apiKey:'hogehoge'})
-});
 
-electron.ipcRenderer.on('async-get-myself-reply', function(event, arg) {
-    ReactDOM.render(<div>{arg}</div>, document.getElementById("app"))
-});
+ReactDOM.render(<div>This is React component</div>, document.getElementById("app"))
