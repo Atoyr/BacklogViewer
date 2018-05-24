fccd304d68c7021ada10508ed25019a3b63c6c32
Atoyr <lessironglance@gmail.com>
2018-05-24 14:53:31 +0900

    fix:フォルダ構成を変更 (#5)

--- app/containers/main.jsx
+++ app/containers/main.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as electron from 'electron'
+import Moc from'../components/moc.jsx';
+
 ReactDOM.render(<div><button id='bt'>GET IT</button></div>, document.getElementById("app"))
 const bt = document.getElementById('bt');
 bt.addEventListener('click',function(){
@@ -10,3 +12,8 @@ bt.addEventListener('click',function(){
 electron.ipcRenderer.on('async-get-myself-reply', function(event, arg) {
     ReactDOM.render(<div>{arg}</div>, document.getElementById("app"))
 });
+
+ReactDOM.render(
+    <Moc />,
+    document.getElementById("app")
+  );
