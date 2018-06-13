1db90a1a8b0baa200ceb025abb9315fc541b3c55
r.uchiyama <r.uchiyama@sint.co.jp>
2018-06-13 18:00:02 +0900

    fix:不要なImportを削除、フッター追加

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import CreateReact from 'create-react-class';
-import {bindActionCreators } from 'redux';
-import {Grid, Row, Col} from 'react-bootstrap';
-import {Button,ButtonGroup,} from 'react-bootstrap';
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
 import {Panel} from 'react-bootstrap';
-import {Form, FormGroup, FormControl, ControlLabel, HelpBlock, HelpBlockProps} from 'react-bootstrap';
-import {Tab,NavItem,Nav} from 'react-bootstrap'
-import {loadSetting, saveSetting} from '../actions'
-import { start } from 'repl';
 
 const card = ({id,summary,startDate,dueDate,assigneeName}) =>(
   <div>
@@ -18,10 +8,12 @@ const card = ({id,summary,startDate,dueDate,assigneeName}) =>(
         <Panel.Title>{summary}</Panel.Title>
       </Panel.Heading>
       <Panel.Body>
-        <p>Start : {(new Date(startDate)).toLocaleDateString("ja-JP")}</p>
-        <p>DueDate : {(new Date(dueDate)).toLocaleDateString("ja-JP")}</p>
-        <p>Assign At : {assigneeName} </p>
+        <p>開始日 : {(new Date(startDate)).toLocaleDateString("ja-JP")}</p>
+        <p>期日 　: {(new Date(dueDate)).toLocaleDateString("ja-JP")}</p>
       </Panel.Body>
+      <Panel.Footer>
+        <p>担当者 : {assigneeName} </p>
+      </Panel.Footer>
     </Panel>
   </div>
 )
