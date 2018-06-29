93fa0dd3a9ac531e8f18aea5386ce11c811f4a51
r.uchiyama <r.uchiyama@sint.co.jp>
2018-06-29 09:45:29 +0900

    feat:ユーザ情報のリフレッシュ処理追加？

--- app/reducers/userInfo.js
+++ app/reducers/userInfo.js
@@ -8,7 +8,7 @@ export default handleActions({
         if(Array.isArray(action.payload))
         {
             const userIds = action.payload.map(x => x.userId)
-            const tempstate = state.filter(x => !userIds.indexOf(x.userId))
+            const tempstate = state.filter(x => (userIds.indexOf(x.userId) == -1))
             newstate = tempstate.concat(action.payload)
         }else{
             newstate = state.filter(x => x.userId != action.payload.userId);
@@ -16,6 +16,17 @@ export default handleActions({
         }
         newstate.sort((a,b) => a.userId < b.userId ? -1 : 1);
         console.log(newstate)
+        return newstate;
+    },
+    REFRESH_USER_INFO:(state,action) =>{
+        var newstate = [];
+        if(Array.isArray(action.payload))
+        {
+            newstate = action.payload
+        }else{
+            newstate.push(action.payload);
+        }
+        newstate.sort((a,b) => a.userId < b.userId ? -1 : 1);
         return newstate;
     },
     FAIL_USER_INFO:(state,action) =>{
